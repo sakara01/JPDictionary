77a83733a07c8aa50d442eb2dc935ef2c20a4d1f
Saloni Karanth <salonikaranth01@gmail.com>
2023-03-30 22:02:58 -0400

    Took like 2 hours to add buttons in list activity 🤡

--- app/src/main/java/com/example/easydictionary/ListActivity.kt
+++ app/src/main/java/com/example/easydictionary/ListActivity.kt
@@ -1,13 +1,14 @@
 package com.example.easydictionary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.example.easydictionary.databinding.ActivityListBinding
 
@@ -15,11 +16,16 @@ class ListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityListBinding
     private lateinit var wordList: ArrayList<Word>
     private lateinit var backBtn : ImageButton
+    private lateinit var btnHideWord : Button
+    private lateinit var btnHideDef : Button
+    private lateinit var btnAdd : ImageButton
     private lateinit var listName: EditText
+    private var hideWordClicked: Boolean = false
+    private var hideDefClicked: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Light)
+        setTheme(R.style.Dark)
         binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,6 +33,9 @@ class ListActivity : AppCompatActivity() {
         val definitions = intent.extras?.getStringArray("defList")
 
         backBtn = findViewById(R.id.btnBack)
+        btnHideWord = findViewById(R.id.btnHideWord)
+        btnHideDef = findViewById(R.id.btnHideDef)
+        btnAdd= findViewById(R.id.btnAdd)
         listName = findViewById(R.id.etListName)
 
         wordList = ArrayList()
@@ -43,6 +52,32 @@ class ListActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
             Animatoo.animateSlideRight(this)
+        }
+
+        btnHideWord.setOnClickListener{
+            if (!hideWordClicked) {
+                val rule = findViewById<ImageView>(R.id.JPrule)
+                rule.apply { translationZ = 30F }
+                hideWordClicked = true
+            }
+            else {
+                val rule = findViewById<ImageView>(R.id.JPrule)
+                rule.apply { translationZ = 0F }
+                hideWordClicked = false
+            }
+        }
+
+        btnHideDef.setOnClickListener{
+            if (!hideDefClicked) {
+                val rule = findViewById<ImageView>(R.id.ENrule)
+                rule.apply { translationZ = 30F }
+                hideDefClicked = true
+            }
+            else {
+                val rule = findViewById<ImageView>(R.id.ENrule)
+                rule.apply { translationZ = 0F }
+                hideDefClicked = false
+            }
         }
 
     }
